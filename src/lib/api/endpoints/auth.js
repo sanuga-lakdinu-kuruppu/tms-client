cd7a232c2e038badd2543ead82a52b9b970ea50f
Sanuga Kuruppu <128165236+sanuga-lakdinu-kuruppu@users.noreply.github.com>
2026-02-14 15:11:01 +0530

    add the login API call to the login page

---
+++ src/lib/api/endpoints/auth.js
@@ -0,0 +1,38 @@
+import apiClient from "../axiosConfig";
+
+export default {
+  login: async ({ email, password }) => {
+    const request = {
+      email,
+      password,
+    };
+    const response = await apiClient.post("/v1/auth/login", request);
+    if (response.status === 200) {
+      localStorage.setItem("accessToken", response.data.data.accessToken);
+      localStorage.setItem("refreshToken", response.data.data.refreshToken);
+      apiClient.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${response.data.data.accessToken}`;
+    }
+    return response;
+  },
+
+  logout: async () => {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    delete apiClient.defaults.headers.common["Authorization"];
+    return { success: true };
+  },
+
+  refreshToken: async (refreshToken) => {
+    const response = await apiClient.post("/v1/auth/refresh", { refreshToken });
+    if (response.data.data.accessToken && response.data.data.refreshToken) {
+      localStorage.setItem("accessToken", response.data.data.accessToken);
+      localStorage.setItem("refreshToken", response.data.data.refreshToken);
+      apiClient.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${response.data.data.accessToken}`;
+    }
+    return response;
+  },
+};
